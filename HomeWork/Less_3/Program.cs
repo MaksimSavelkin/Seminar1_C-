e63f4ef8333a965f57ab86063c6a3ed23fe1bdcc
msavelkin <msavelkin@gmail.com>
2022-07-13 14:16:43 +0300

    v.3.4.5

--- HomeWork/Less_3/Program.cs
+++ HomeWork/Less_3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+
+if (a == 1)
+{
+    Console.WriteLine("Пн");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Вт");
+}
+else if (a == 3)
+{
+    Console.WriteLine("Ср");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Чт");
+}
+else if (a == 5)
+{
+    Console.WriteLine("Пт");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Сб");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Вс");
+}
+
---
+++ HomeWork/Less_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else if  (b > c)
+{
+    Console.WriteLine(b);
+}
+else if (c > a)
+{
+    Console.WriteLine(c);
+}
+
+
