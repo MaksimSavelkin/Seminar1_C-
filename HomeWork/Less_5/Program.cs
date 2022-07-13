3c03db06f9409d32dd1dad90e89d472e43f1b4d9
msavelkin <msavelkin@gmail.com>
2022-07-13 15:27:36 +0300

    V5

--- HomeWork/Less_5/Program.cs
+++ HomeWork/Less_5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе 
+// показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+if (a < 0)
+int i=-N;
+while (i <= N)
+{
+    Console.WriteLine(i);
+    i++;
+}
