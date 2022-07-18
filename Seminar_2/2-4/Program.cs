cc6cc02591b1f8e73324d9345263f4b736f58972
msavelkin <msavelkin@gmail.com>
2022-07-18 14:49:23 +0300

    Task 2.3

---
+++ Seminar_2/2-4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно 7 и 23.
+void Method (int a)
+{
+    if (a % 7 == 0)
+        if (a % 23 == 0)
+            Console.WriteLine("Кратно");
+        else
+            Console.WriteLine("Не кратно");
+    else 
+        Console.WriteLine("Не кратно");
+}
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+
+Method(a);
