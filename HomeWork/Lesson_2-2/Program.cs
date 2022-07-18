3d693c6a9b170c7b5531204dd169b9c643a318bf
msavelkin <msavelkin@gmail.com>
2022-07-18 15:44:54 +0300

    HomeWork 2-2

---
+++ HomeWork/Lesson_2-2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+void Method2(int a)
+{
+    Console.Write(a / 100);
+    Console.Write(a % 10);
+    Console.WriteLine(" ");
+    
+}
+int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+Method2(a);
