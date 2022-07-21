3380a8fa62fc4a85b525ab54f18608624281adf5
msavelkin <msavelkin@gmail.com>
2022-07-21 14:14:26 +0300

    Hom 3.2

--- HomeWork/Lesson_3-3/Program.cs
+++ HomeWork/Lesson_3-3/Program.cs
@@ -1,4 +1,15 @@
 ﻿// Задача 3: Напишите программу, которая принимает на
 // вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-Console.WriteLine("Hello, World!");
+void Cvadrat (int N)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+        i ++;
+
+    }
+
+}
+Cvadrat(5);
