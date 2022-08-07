c1ef5de487e296d15a7260fb9dd446a7534c1f94
msavelkin <msavelkin@gmail.com>
2022-08-07 22:57:09 +0300

    HomeWork 4-1

--- HomeWork/Lesson_4-3/Program.cs
+++ HomeWork/Lesson_4-3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая задаёт массив из
+// 8 элементов случайными числами и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции
+// (пригодится в следующих задачах)
+
+int[] arr(int num)
+{
+    int[] array = new int[num];
+    for(int i=0; i < num; i++)
+    {
+        array[i]=new Random().Next(0, 10);
+    }
+    return array;
+}
+arr(8);
+
+void PrintArray (int[] array2)
+{
+    for(int i=0; i < array2.Length; i++)
+        Console.WriteLine($"{array2[i]} ");
+}
+PrintArray(arr(8));
