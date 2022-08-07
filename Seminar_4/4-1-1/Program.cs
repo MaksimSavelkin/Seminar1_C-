445dd7833bf2323bdf15f366989535346e90c1bc
msavelkin <msavelkin@gmail.com>
2022-08-07 22:12:15 +0300

    Seminar 4-2

---
+++ Seminar_4/4-1-1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+void Leng(int num)
+{
+    string num1 = num.ToString();
+    Console.WriteLine(num1.Length);
+}
+
+Leng(4521);
--- Seminar_4/4-2/Program.cs
+++ Seminar_4/4-2/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
