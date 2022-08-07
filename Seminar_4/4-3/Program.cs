a9249cf8f810f178a1e43f4aa7718361166bc530
msavelkin <msavelkin@gmail.com>
2022-08-07 22:29:44 +0300

    Seminar 4-4

--- Seminar_4/4-3/Program.cs
+++ Seminar_4/4-3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая выводит
+// массив из 8 элементов, заполненный нулями и
+// единицами в случайном порядке.
+void Rundom01Array(int num)
+{
+    int[] array = new int[num];
+    for(int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write($"{array[i]}");
+    }
+}
+Rundom01Array(25);
