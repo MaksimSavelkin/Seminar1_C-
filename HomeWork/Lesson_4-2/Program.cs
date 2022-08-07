d2777cfe1b8de1f66c65cb8e257f3ef850acea8c
msavelkin <msavelkin@gmail.com>
2022-08-07 22:55:26 +0300

    HomeWork 4-2

--- HomeWork/Lesson_4-2/Program.cs
+++ HomeWork/Lesson_4-2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+
+int sum = 0;
+while (a > 0) 
+{
+    sum += a % 10;
+    a /= 10;
+}
+
+Console.WriteLine (sum);
