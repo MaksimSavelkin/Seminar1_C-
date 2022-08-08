6cb3e0e0f446161aa4e92251d1db4d397f7b8669
msavelkin <msavelkin@gmail.com>
2022-08-08 21:46:25 +0300

    Seminar5-1d

--- Seminar_5/5-1demo/Program.cs
+++ Seminar_5/5-1demo/Program.cs
@@ -1,5 +1,42 @@
 ﻿// Задача 1: Задайте массив из 12 элементов, заполненный
 // случайными числами из промежутка [-9, 9]. Найдите сумму
 // отрицательных и положительных элементов массива.
+void Print(int[] arr)
+{
+    int size = arr.Length;
 
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine();
+}
 
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for(int i = 0; i < size; i ++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void SumPosNeg(int[] arr)
+{
+    int pos, neg;
+    pos = neg = 0;
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= 0)
+            pos += arr[i];
+        else
+            neg += arr[i];   
+    }
+    Console.WriteLine($"Pos: {pos}, neg: {neg}");
+}
+int[] arr_1 = MassNums(12, -9, 10);
+Print(arr_1);
+SumPosNeg(arr_1);
