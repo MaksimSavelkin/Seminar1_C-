0820c75a5d7514f15b39e540808baa9b3a833d54
msavelkin <msavelkin@gmail.com>
2022-08-10 22:21:07 +0300

    Seminar 5-3

--- Seminar_5/5-2/Program.cs
+++ Seminar_5/5-2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for(int i = 0; i < size; i ++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void Perebor(int[] arr, int num)
+{
+    
+    for(int i = 0; i < arr.Length; i++)
+        if(arr[i] == num)
+            Console.WriteLine("Да");
+        else
+            Console.WriteLine("Нет");
+               
+    
+}
+int[] arr_1 = MassNums(12, -9, 10);
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Print(arr_1);
+Perebor(arr_1, num);
+
