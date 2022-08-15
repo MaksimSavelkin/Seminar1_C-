b4952e1dfcae5260be40bd9a35e64b92ada1ac31
msavelkin <msavelkin@gmail.com>
2022-08-15 21:38:36 +0300

    Lesson 6-2

--- HomeWork/Lesson_6-2/Program.cs
+++ HomeWork/Lesson_6-2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
+// b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+Console.WriteLine("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+double q=b2-b1;
+double w = k1-k2;
+double x = q/w;
+
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения [{x}, {y}]");
