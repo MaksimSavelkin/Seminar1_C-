a5acc29ba473e0bdc5788c818c00fbddd4867406
msavelkin <msavelkin@gmail.com>
2022-08-22 22:16:11 +0300

    Home Work 9-2

--- HomeWork/Lesson_9-2/Program.cs
+++ HomeWork/Lesson_9-2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+void SumNum (int m, int n, int sum)
+{
+    if (n >= m) 
+    {
+        sum = sum + (m++);
+        SumNum(m, n, sum);
+        return;
+    }
+    Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+}
+
+Console.WriteLine("Введите начальное число M:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int n = int.Parse(Console.ReadLine());
+
+SumNum(m, n, 0);
