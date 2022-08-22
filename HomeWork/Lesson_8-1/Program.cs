5cc4453b1eca36faae26d6f77d426521d014fe18
msavelkin <msavelkin@gmail.com>
2022-08-22 21:40:22 +0300

    Home Work 8-1

--- HomeWork/Lesson_8-1/Program.cs
+++ HomeWork/Lesson_8-1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+void SelectionSort(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int count = i; count < arr.GetLength(1) - j - 1; count++)
+            {
+                if (arr[i, count] < arr[i, count + 1])
+                {
+                  (arr[i, count], arr[i, count + 1]) = (arr[i, count + 1], arr[i, count]);
+                }
+            }            
+        }
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 0, 10);
+Print(arr_1);
+SelectionSort(arr_1);
+Print(arr_1);
